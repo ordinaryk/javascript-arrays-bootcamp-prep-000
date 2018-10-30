b71d45a894946744092a7055ab8a249f286f6836
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-30 23:11:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,39 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kit kat', 'skittles']
+
+
+function addElementToBeginningOfArray(array, element) {
+  return [array,...element]
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
+
+function addElementToEndOfArray(array, element) {
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
+
+function accessElementInArray(array, index) {
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
+
+function removeElementFromBeginningOfArray(array) {
+  
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  
+}
+
+function removeElementFromEndOfArray(array) {
+  
+}
