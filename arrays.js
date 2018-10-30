71b3664919585975ac3f22c8e5a4e24fe2d67e22
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-30 23:16:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kit kat', 'skittles']
 
 
 function addElementToBeginningOfArray(array, element) {
-  return [array,...element]
-
+  a = [array,...element]
+  return a
 }
