577ee033975e9502ce921a0e5d8b9cc95772557d
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-30 23:36:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-   array.unshift(element)
-   return array
+   a = array.unshift(element)
+   return a
   }
