c4e94fe9f33980bf37ac5b3b535f464d3607b1c4
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-30 23:22:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   a = [element,...array]
   return a
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.push(element)
+}
