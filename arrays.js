e9e8f6b04efca515f6f49a2fc7e388269b33e290
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-31 00:52:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,6 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  a = array.pop()
-  return a
+  array.pop()
+  return array
 }
